ec25728c1bb0815153d33d140f89c3aec1f7ec26
rafaelbetatester <rafaelbetatester@gmail.com>
2016-09-09 18:35:49 -0300

    New

--- reaction.cpp
+++ reaction.cpp
@@ -1,12 +1,12 @@
 #include <stdio.h>
-#include <string> 
+#include <string>
 #include <stdlib.h>
 #include <sstream>
 #include <string.h>
-#define width 200
-#define height 200
-#define dA 1
-#define dB 0.5
+#define width 5
+#define height 5
+#define dA 0.1
+#define dB 0.05
 #define f 0.055
 #define k 0.062
 #define dT 1
@@ -30,14 +30,18 @@ void initialize()
   for (int i = 0; i < width; i++)
     for (int j = 0; j < height; j++)
       {
-	grid[i][j].a = 1;
-	grid[i][j].b = 0;
-	next[i][j].a = 1;
-	next[i][j].b = 0;
-       }
-  for (int i = 100; i < 110; i++)
-    for (int j = 100; j < 110; j++)
-      grid[i][j].b = 1;
+        grid[i][j].a = 1;
+        grid[i][j].b = 0;
+        next[i][j].a = 1;
+        next[i][j].b = 0;
+      }
+  for (int i = 1; i < 3; i++)
+    for (int j = 1; j < 3; j++)
+      {
+	grid[i][j].b = 1;
+	grid[i][j].a = 0.1;
+      }
+
 }
 
 void printMap(int t)
@@ -48,13 +52,13 @@ void printMap(int t)
   ss << t;
   string str = ss.str();
   FILE *fin = fopen(("./output/"+str+".txt").c_str(),"w+");
-    for (int i = 0; i < width; i++)
-      {
+  for (int i = 0; i < width; i++)
+    {
       for (int j = 0; j < height; j++)
-	fprintf(fin,"%lf ", grid[i][j].b);
+        fprintf(fin,"%lf ", grid[i][j].b);
       fprintf (fin,"\n");
-      } 
-    fclose(fin);
+    }
+  fclose(fin);
 }
 
 void next_generation()
@@ -63,7 +67,7 @@ void next_generation()
     for (int j = 0; j < height; j++)
       {
         grid[i][j].a = next[i][j].a;
-	grid[i][j].b = next[i][j].b;
+        grid[i][j].b = next[i][j].b;
       }
 
 }
@@ -71,55 +75,61 @@ void next_generation()
 
 double laplaceA(int x, int y)
 {
-  double sumA = 0;
-  sumA+=grid[x][y].a * -1;
-  sumA+=grid[x-1][y].a * 0.2;
-  sumA+=grid[x+1][y].a * 0.2;
-  sumA+=grid[x][y+1].a * 0.2;
-  sumA+=grid[x][y-1].a * 0.2;
-  sumA+=grid[x+1][y+1].a * 0.05;
-  sumA+=grid[x-1][y+1].a * 0.05;
-  sumA+=grid[x+1][y-1].a * 0.05;
-  sumA+=grid[x-1][y-1].a * 0.05;
+  double sumA = -4*grid[x][y].a;
+  int dX[] = {1,1,-1,-1};
+  int dY[] = {1,-1,1,-1};
+  for (int i = 0; i < 4; i ++)
+    {
+      int pX = x + dX[i];
+      int pY = y + dY[i];
+      if (pX < 0 || pX >= width)
+        pX = 2*x - pX;
+      if (pY < 0 || pY >= height)
+        pY = 2*y - pY;
+      sumA += grid[pX][pY].a;
+    }
+
   return sumA;
 
 }
 
 double laplaceB(int x,int y)
 {
-
-  double sumB = 0;
-  sumB+=grid[x][y].b * -1;
-  sumB+=grid[x-1][y].b * 0.2;
-  sumB+=grid[x+1][y].b * 0.2;
-  sumB+=grid[x][y+1].b * 0.2;
-  sumB+=grid[x][y-1].b * 0.2;
-  sumB+=grid[x+1][y+1].b * 0.05;
-  sumB+=grid[x-1][y+1].b * 0.05;
-  sumB+=grid[x+1][y-1].b * 0.05;
-  sumB+=grid[x-1][y-1].b * 0.05;
-
+  double sumB = -4*grid[x][y].b;
+  int dX[] = {1,-1,0,0};
+  int dY[] = {0,0,1,-1};
+  for (int i = 0; i < 4; i ++)
+    {
+      int pX = x + dX[i];
+      int pY = y + dY[i];
+      if (pX < 0 || pX >= width)
+        pX = 2*x - pX;
+      if (pY < 0 || pY >= height)
+        pY = 2*y - pY;
+      sumB += grid[pX][pY].b;
+    }
   return sumB;
 }
 
 int main()
 {
   initialize();
-  
-  while(T <= 10)
+
+  while(T <= 1000)
     {
       T = T + dT;
-      for (int x = 1; x < width-1; x++)
-	for (int y = 1; y < height-1; y++)
-	  {
-	    double a = grid[x][y].a;
-	    double b = grid[x][y].b;
-	    next[x][y].a = a + dA*laplaceA(x,y) - a*b*b +  f*(1-a);
-	    next[x][y].b = b + dB*laplaceB(x,y) + a*b*b - (k+f)*b;
-		    
-	  }
-      next_generation();
+      for (int x = 0; x < width; x++)
+        for (int y = 0; y < height; y++)
+          {
+            double a = grid[x][y].a;
+            double b = grid[x][y].b;
+            next[x][y].a = a + dA*laplaceA(x,y) - a*b*b +  f*(1-a);
+            next[x][y].b = b + dB*laplaceB(x,y) + a*b*b - (k+f)*b;
+
+          }
+      //      next_generation();
       printMap(T);
+      next_generation();
     }
   return 0;
 }
